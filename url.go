0e1af638709cba8e9a8af71759a9c641be6c93ff
Alisson Darosci <alisson_darosci@hotmail.com>
2020-04-13 16:22:52 -0300

    atualização de urls

--- url.go
+++ url.go
@@ -2,9 +2,9 @@ package client
 
 // urls
 const (
-	SocketURL     = "ws://localhost:3336/sync"
-	ConnectURL    = "http://localhost:3336/connect"
-	AuthURL       = "http://localhost:3336/auth"
-	DisconnectURL = "http://localhost:3336/disconnect"
-	ConfirmURL    = "http://localhost:3336/confirm"
+	SocketURL     = "wss://socket.brzomessages.com/sync"
+	ConnectURL    = "https://socket.brzomessages.com/connect"
+	AuthURL       = "https://socket.brzomessages.com/auth"
+	DisconnectURL = "https://socket.brzomessages.com/disconnect"
+	ConfirmURL    = "https://socket.brzomessages.com/confirm"
 )
